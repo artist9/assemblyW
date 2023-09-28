d41fd2bbea28e531153a944743d81873ebdc76ae
Artist <krlmnhzn@gmail.com>
2023-09-28 14:05:47 +0300

    Pug3 and pug-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const PugPlugin = require('pug-plugin');
 const path = require('path');
 const FileManagerPlugin = require('filemanager-webpack-plugin');
 
 module.exports = {
-	entry: path.join(__dirname, 'src', 'index.js'),
+	entry: {
+	index: './src/index.pug',
+	'pages/about': './src/about/index.pug',
+	//path.join(__dirname, 'src', 'index.js'),
+	},
 	output: {
 		path: path.join(__dirname, 'dist'),
 		filename: 'index.[contenthash].js',
@@ -15,12 +19,25 @@ module.exports = {
 				use: 'babel-loader',
 				exclude: /node_modules/,
 			},
+			{
+				test: /\.pug$/,
+				loader: PugPlugin.loader,
+			},
+			{
+				test: /\.(css|sass|scss)$/,
+				use: ['css-loader', 'sass-loader'],
+			},
 		],
 	},
 	plugins: [
-		new HtmlWebpackPlugin({
-			template: path.join(__dirname, 'src', 'template.html'),
-			filename: 'index.html',
+		new PugPlugin({
+			pretty: true,
+			js: {
+				filename: 'assets/js/[name].[contenthash:8].js',
+			},
+			css: {
+				filename: 'assets/css/[name].[contenthash:8].css',
+			},
 		}),
 		new FileManagerPlugin({
 			events: {
