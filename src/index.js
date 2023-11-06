d5a615131f6684d1f0b78822f03790844caa54c2
Artist <krlmnhzn@gmail.com>
2023-11-06 17:50:13 +0200

    Pug3 and React

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './components/app';
+
 function component(text) {
 	const element = document.createElement('h1');
 	element.textContent = text;
 	return element;
 }
 document.body.prepend(component('Проект собран на Webpack'));
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
